aa53d1c9731c1ee602e1615c65f41867b690b92a
Viacheslav Voytenko <calorifer75@yandex.ru>
2021-08-08 13:25:10 +0300

    Компонент Techs

--- src/components/AboutProject/AboutProject.js
+++ src/components/AboutProject/AboutProject.js
@@ -4,17 +4,17 @@ function AboutProject() {
   return (
     <section className='about-project' id='about-project'>
       <div className='about-project__wrapper'>
-        <h2 className='about-project__title about-project__title_underlined'>
+        <h2 className='main-subtitle main-subtitle_underlined'>
           О проекте
         </h2>
-        <h2 className='about-project__title'>
+        <h2 className='main-subtitle'>
           Дипломный проект включал 5 этапов
         </h2>
         <p className='about-project__paragraph'>
           Составление плана, работу над бэкендом, вёрстку, добавление
           функциональности и финальные доработки.
         </p>
-        <h2 className='about-project__title'>
+        <h2 className='main-subtitle'>
           На выполнение диплома ушло 5 недель
         </h2>
         <p className='about-project__paragraph'>
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,6 +2,7 @@ import './Main.css';
 import Promo from '../Promo/Promo';
 import Header from '../Header/Header';
 import AboutProject from '../AboutProject/AboutProject';
+import Techs from '../Techs/Techs';
 
 function Main() {
   return (
@@ -9,6 +10,7 @@ function Main() {
       <Header />
       <Promo />
       <AboutProject />
+      <Techs />
     </>
   );
 }
