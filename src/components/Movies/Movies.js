6823e8cd897a48430146ff60f7643f477a8c657f
Viacheslav Voytenko <calorifer75@yandex.ru>
2021-08-10 15:36:31 +0300

    Компонент MoviesCardList

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,12 +1,14 @@
 import './Movies.css';
 import Header from '../Header/Header';
 import SearchForm from '../SearchForm/SearchForm';
+import MoviesCardList from '../MoviesCardList/MoviesCardList';
 
 function Movies() {
   return (
     <>
       <Header />
       <SearchForm />
+      <MoviesCardList />
     </>
   );
 }
---
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -0,0 +1,13 @@
+import './MoviesCardList.css';
+
+function MoviesCardList() {
+  return (
+    <section className='movies-card-list'>
+      <div className='movies-card-list__wrapper'>
+        
+      </div>
+    </section>
+  )
+}
+
+export default MoviesCardList;
