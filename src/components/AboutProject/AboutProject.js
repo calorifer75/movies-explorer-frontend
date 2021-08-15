c6c1f702552067e8ec4de1221ec553d90f0bcce1
Viacheslav Voytenko <calorifer75@yandex.ru>
2021-08-15 11:28:56 +0300

    Верстка Main 768

--- src/components/AboutProject/AboutProject.js
+++ src/components/AboutProject/AboutProject.js
@@ -4,23 +4,28 @@ function AboutProject() {
   return (
     <section className='about-project' id='about-project'>
       <div className='about-project__wrapper'>
-        <h2 className='main-subtitle main-subtitle_underlined'>
-          О проекте
-        </h2>
-        <h2 className='main-subtitle'>
-          Дипломный проект включал 5 этапов
-        </h2>
-        <p className='about-project__paragraph'>
-          Составление плана, работу над бэкендом, вёрстку, добавление
-          функциональности и финальные доработки.
-        </p>
-        <h2 className='main-subtitle'>
-          На выполнение диплома ушло 5 недель
-        </h2>
-        <p className='about-project__paragraph'>
-          У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было
-          соблюдать, чтобы успешно защититься.
-        </p>
+        <h2 className='main-subtitle main-subtitle_underlined'>О проекте</h2>
+
+        <div className='about-project__row'>
+          <div className='about-project__column'>
+            <h2 className='main-subtitle'>Дипломный проект включал 5 этапов</h2>
+            <p className='about-project__paragraph'>
+              Составление плана, работу над бэкендом, вёрстку, добавление
+              функциональности и финальные доработки.
+            </p>
+          </div>
+
+          <div className='about-project__column'>
+            <h2 className='main-subtitle'>
+              На выполнение диплома ушло 5 недель
+            </h2>
+            <p className='about-project__paragraph'>
+              У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было
+              соблюдать, чтобы успешно защититься.
+            </p>
+          </div>
+        </div>
+
         <div className='about-project__timeline'>
           <div className='about-project__backend-wrapper'>
             <div className='about-project__backend-box'>1 неделя</div>
